943faff77f7c7325e4ec6fd6cbcc125671dfc121
hikee9123 <hikee9123@gmail.com>
2022-01-02 14:57:15 +0900

    test

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -239,7 +239,7 @@ class CarController():
 
     if frame == 0: # initialize counts from last received count signals
       self.lkas11_cnt = CS.lkas11["CF_Lkas_MsgCount"] + 1
-      self.scc12_cnt = CS.scc12["CR_VSM_Alive"] + 1  #if not CS.no_radar else 0
+      self.scc12_cnt = CS.scc12["CR_VSM_Alive"] + 1  #if self.scc_live else 0
 
     self.lkas11_cnt %= 0x10
     self.scc12_cnt %= 0xF
