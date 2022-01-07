a33aec108b395609c9378570c97839e0a5858c49
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2022-01-07 19:29:52 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -161,12 +161,11 @@ class CarController():
     accel = clip(accel, CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX)
 
     if (CS.aReqValue > accel) and accel < 0 and CS.clu_Vanz > 3:
-      #can_sends.append( create_scc12(self.packer, accel, enabled, self.scc12_cnt, self.scc_live, CS.scc12 ) )
-      pass
+      can_sends.append( create_scc12(self.packer, accel, enabled, self.scc12_cnt, self.scc_live, CS.scc12 ) )
     else:
       accel = CS.aReqValue
 
-    can_sends.append( create_scc12(self.packer, accel, enabled, self.scc12_cnt, self.scc_live, CS.scc12 ) )
+    #can_sends.append( create_scc12(self.packer, accel, enabled, self.scc12_cnt, self.scc_live, CS.scc12 ) )
     self.accel = accel
     return can_sends    
 
@@ -247,7 +246,7 @@ class CarController():
 
     if frame == 0: # initialize counts from last received count signals
       self.lkas11_cnt = CS.lkas11["CF_Lkas_MsgCount"] + 1
-      self.scc12_cnt =  CS.scc12["CR_VSM_Alive"] + 1
+      self.scc12_cnt = CS.scc12["CR_VSM_Alive"] + 1
   
     self.lkas11_cnt %= 0x10
     self.scc12_cnt %= 0x0F
