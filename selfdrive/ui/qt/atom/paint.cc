18916c57e13a0bb7f80d5884bc6f269bb81c9d6f
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2022-01-31 12:00:30 +0900

    Update paint.cc

--- selfdrive/ui/qt/atom/paint.cc
+++ selfdrive/ui/qt/atom/paint.cc
@@ -320,7 +320,7 @@ void OnPaint::bb_ui_draw_measures_right( QPainter &p, int bb_x, int bb_y, int bb
 
     // temp is alway in C * 1000
     val_str.sprintf("%.1f", m_param.batteryTemp );
-    uom_str.sprintf("%.0f" fan_speed * 0.001);        
+    uom_str.sprintf("%.0f", fan_speed * 0.001);        
     bb_h +=bb_ui_draw_measure(p,  val_str, uom_str, "BAT TEMP",
         bb_rx, bb_ry, bb_uom_dx,
         val_color, lab_color, uom_color,
