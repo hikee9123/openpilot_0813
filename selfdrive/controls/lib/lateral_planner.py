17c4396c33174bdbfc5e21a2909fcc9556817b96
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2022-02-02 16:57:09 +0900

    Update lateral_planner.py

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -32,6 +32,7 @@ class LateralPlanner:
 
     # atom
     self.time_laneline = 0
+    self.lane_lines  = self.use_lanelines
 
   def lanelines_check(self, sm):
     lanelines = self.use_lanelines
@@ -47,7 +48,7 @@ class LateralPlanner:
         lanelines = False
         
         
-        print(' r={}   l= {}    lanelines ={} \n '.format(self.LP.rll_prob, self.LP.lll_prob,  lanelines) )
+        #print(' r={}   l= {}    lanelines ={} \n '.format(self.LP.rll_prob, self.LP.lll_prob,  lanelines) )
 
     return lanelines
 
@@ -83,7 +84,8 @@ class LateralPlanner:
 
     # Calculate final driving path and set MPC costs
     #if self.use_lanelines:
-    if self.lanelines_check(sm):
+    self.lane_lines   = self.lanelines_check(sm)
+    if self.lane_lines:
       d_path_xyz = self.LP.get_d_path(v_ego, self.t_idxs, self.path_xyz)
       self.lat_mpc.set_weights(MPC_COST_LAT.PATH, MPC_COST_LAT.HEADING, self.steer_rate_cost)
     else:
@@ -142,7 +144,7 @@ class LateralPlanner:
     lateralPlan.solverExecutionTime = self.lat_mpc.solve_time
 
     lateralPlan.desire = self.DH.desire
-    lateralPlan.useLaneLines = not self.use_lanelines
+    lateralPlan.useLaneLines = not self.lane_lines
     lateralPlan.laneChangeState = self.DH.lane_change_state
     lateralPlan.laneChangeDirection = self.DH.lane_change_direction
     lateralPlan.modelSpeed = float(self.LP.soft_model_speed)
